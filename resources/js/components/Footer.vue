1704ce78ba2dc5d8a0b95392fd084045783c3a06
Masahiro Harada <masahiro.harada@ari-jp.com>
2019-01-07 22:30:58 +0900

    ログアウト機能追加

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,8 +1,20 @@
 <template>
   <footer class="footer">
-    <button class="button button--link">Logout</button>
+    <button class="button button--link" @click="logout">Logout</button>
     <RouterLink class="button button--link" to="/login">
       Login / Register
     </RouterLink>
   </footer>
 </template>
+
+<script>
+export default {
+  methods: {
+    async logout () {
+      await this.$store.dispatch('auth/logout')
+
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -18,6 +18,10 @@ const actions = {
   async login (context, data) {
     const response = await axios.post('/api/login', data)
     context.commit('setUser', response.data)
+  },
+  async logout (context) {
+    const response = await axios.post('/api/logout')
+    context.commit('setUser', null)
   }
 }
 

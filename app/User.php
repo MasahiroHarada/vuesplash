45cfebfa097ebc90429b5c9df81b3bcb54a17aea
Masahiro Harada <masahiro.harada@ari-jp.com>
2019-01-09 11:55:51 +0900

    写真投稿API

--- app/User.php
+++ app/User.php
@@ -27,4 +27,13 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * リレーションシップ - photosテーブル
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function photos()
+    {
+        return $this->hasMany('App\Photo');
+    }
 }

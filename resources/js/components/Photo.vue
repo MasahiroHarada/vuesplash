cbb423b2349960832cd3da5ad6595a93e257d854
Masahiro Harada <masahiro.harada@ari-jp.com>
2019-01-10 16:24:27 +0900

    写真一覧表示フロントエンド

---
+++ resources/js/components/Photo.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="photo">
+    <figure class="photo__wrapper">
+      <img
+        class="photo__image"
+        :class="imageClass"
+        :src="item.url"
+        :alt="`Photo by ${item.owner.name}`"
+        @load="setAspectRatio"
+        ref="image"
+      >
+    </figure>
+    <RouterLink
+      class="photo__overlay"
+      :to="`/photos/${item.id}`"
+      :title="`View the photo by ${item.owner.name}`"
+    >
+      <div class="photo__controls">
+        <button
+          class="photo__action photo__action--like"
+          title="Like photo"
+        >
+          <i class="icon ion-md-heart"></i>12
+        </button>
+        <a
+          class="photo__action"
+          title="Download photo"
+          download
+          :href="item.url"
+        >
+          <i class="icon ion-md-arrow-round-down"></i>
+        </a>
+      </div>
+      <div class="photo__username">
+        {{ item.owner.name }}
+      </div>
+    </RouterLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      landscape: false,
+      portrait: false
+    }
+  },
+  computed: {
+    imageClass () {
+      return {
+        // 横長クラス
+        'photo__image--landscape': this.landscape,
+        // 縦長クラス
+        'photo__image--portrait': this.portrait
+      }
+    }
+  },
+  methods: {
+    setAspectRatio () {
+      if (! this.$refs.image) {
+        return false
+      }
+      const height = this.$refs.image.clientHeight
+      const width = this.$refs.image.clientWidth
+      // 縦横比率 3:4 よりも横長の画像
+      this.landscape = height / width <= 0.75
+      // 横長でなければ縦長
+      this.portrait = ! this.landscape
+    }
+  },
+  watch: {
+    $route () {
+      // ページが切り替わってから画像が読み込まれるまでの間に
+      // 前のページの同じ位置にあった画像の表示が残ってしまうことを防ぐ
+      this.landscape = false
+      this.portrait = false
+    }
+  }
+}
+</script>
--- resources/js/pages/PhotoList.vue
+++ resources/js/pages/PhotoList.vue
@@ -1,3 +1,62 @@
 <template>
-  <h1>Photo List</h1>
+  <div class="photo-list">
+    <div class="grid">
+      <Photo
+        class="grid__item"
+        v-for="photo in photos"
+        :key="photo.id"
+        :item="photo"
+      />
+    </div>
+    <Pagination :current-page="currentPage" :total="total" />
+  </div>
 </template>
+
+<script>
+import { OK } from '../util'
+import Photo from '../components/Photo.vue'
+import Pagination from '../components/Pagination.vue'
+
+export default {
+  components: {
+    Photo,
+    Pagination
+  },
+  props: {
+    page: {
+      type: Number,
+      required: false,
+      default: 1
+    }
+  },
+  data () {
+    return {
+      photos: [],
+      currentPage: 0,
+      total: 0
+    }
+  },
+  methods: {
+    async fetchPhotos () {
+      const response = await axios.get(`/api/photos/?page=${this.page}`)
+
+      if (response.status !== OK) {
+        this.$store.commit('error/setCode', response.status)
+        return false
+      }
+
+      this.photos = response.data.data
+      this.currentPage = response.data.current_page
+      this.total = response.data.total
+    }
+  },
+  watch: {
+    $route: {
+      async handler () {
+        await this.fetchPhotos()
+      },
+      immediate: true
+    }
+  }
+}
+</script>

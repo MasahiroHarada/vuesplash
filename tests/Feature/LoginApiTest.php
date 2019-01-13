bf806fc6c645bf8d35b989b8102c6a1f30194087
Masahiro Harada <masahiro.harada@ari-jp.com>
2019-01-14 02:59:12 +0900

    認証テストリファクタリング

--- tests/Feature/LoginApiTest.php
+++ tests/Feature/LoginApiTest.php
@@ -2,41 +2,37 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class LoginApiTest extends TestCase
 {
-    use RefreshDatabase, AuthTest;
+    use RefreshDatabase;
 
     public function setUp()
     {
         parent::setUp();
 
-        $this->createUser();
+        // テストユーザー作成
+        $this->user = factory(User::class)->create();
     }
 
     /**
      * @test
      */
-    public function should_登録済みのユーザーを認証する()
+    public function should_登録済みのユーザーを認証して返却する()
     {
-        $response = $this->json('POST', route('login'), $this->credentials());
-
-        $response->assertStatus(200);
-        $this->assertAuthenticatedAs($this->user);
-    }
-
-    /**
-     * @test
-     */
-    public function should_認証済みのユーザーを返却する()
-    {
-        $response = $this->json('POST', route('login'), $this->credentials());
+        $response = $this->json('POST', route('login'), [
+            'email' => $this->user->email,
+            'password' => 'secret',
+        ]);
 
         $response
             ->assertStatus(200)
             ->assertJson(['name' => $this->user->name]);
+
+        $this->assertAuthenticatedAs($this->user);
     }
 }
